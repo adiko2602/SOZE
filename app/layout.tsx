c8946b075e2001931805410a4211789689f34107
Adrian Rudnicki <92043491+adiko2602@users.noreply.github.com>
2023-11-24 00:26:20 +0100

    Create header #9 (#10)
    * use lato font in tailwindcss
    
    * add graphics
    
    * new classes for div looks like button
    
    * header with components
    
    * shadcn components
    
    * brakepoint hook
    
    * add header and lato font
    
    * radix-ui icons and shadcn dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const lato = Lato({
   subsets: ["latin-ext"],
   weight: ["100", "300", "400", "700", "900"],
+  variable: "--font-lato",
 });
 
 export const metadata: Metadata = {
@@ -18,8 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl-PL">
-      <body className={lato.className}>
-        <main className="flex min-h-screen">{children}</main>
+      <body className={`${lato.variable} font-lato`}>
+        <main className="flex min-h-screen flex-col">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
