7c704235a8f3ebaf6133a25693ebbc82517507bd
Adrian Rudnicki <adi2602@wp.pl>
2023-12-07 14:48:02 +0100

    things

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useBrakepoint } from "@/hooks/useBrakepoint";
+import { useBrakepoint } from "@/lib/hooks/useBrakepoint";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
