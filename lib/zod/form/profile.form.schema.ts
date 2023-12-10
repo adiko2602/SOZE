9d83c9b3fdbd0e17c316d0dcd354c91c89a4e4fd
Adrian Rudnicki <adi2602@wp.pl>
2023-12-10 20:56:19 +0100

    profile schema

--- lib/zod/form/profile.form.schema.ts
+++ lib/zod/form/profile.form.schema.ts
@@ -1,29 +1,11 @@
 import * as z from "zod";
 import peselValidation from "../../helpers/peselValidation";
 import { contactSchema } from "../contact.schema";
+import { personalSchema } from "../personal.schema";
+import { addressSchema } from "../address.schema";
 
-export const profilerofileFormSchema = z.object({
-  firstName: z
-    .string({ required_error: "Imię jest wymagane" })
-    .min(1, { message: "Imię nie może być puste" }),
-  lastName: z
-    .string({ required_error: "Nazwisko jest wymagane" })
-    .min(1, { message: "Nazwisko nie może być puste" }),
-  pesel: z
-    .string({ required_error: "PESEL jest wymagany" })
-    .length(11, { message: "PESEL musi mieć 11 cyfr" })
-    .refine(
-      (pesel) => {
-        const regex = /^\d+$/;
-        return regex.test(pesel);
-      },
-      { message: "PESEL musi składać się tylko z cyfr" }
-    )
-    .refine(
-      (pesel) => {
-        return peselValidation(pesel);
-      },
-      { message: "PESEL jest nieprawidłowy" }
-    ),
+export const profileFormSchema = z.object({
   contact: contactSchema,
+  personal: personalSchema,
+  address: addressSchema,
 });
