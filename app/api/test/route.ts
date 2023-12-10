89aae42c2c6a6dd1bee85fff9971897eb0bf1dba
Adrian Rudnicki <adi2602@wp.pl>
2023-12-10 21:00:27 +0100

    remove test

--- app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { Get } from "@/lib/api/serverFunctionResponseToApiResponse";
-import { TTest, getTest } from "@/lib/actions/test";
-
-export async function GET() {
-  return Get<TTest>(await getTest());
-}
